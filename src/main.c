f7d35c1b509d806b2817c50ad2bd852eb35509c5
otavioaugs <tavioaug@proton.me>
2024-02-20 23:03:35 -0300

    basic project structure 2

--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "display.h"
+
 #include <SDL2/SDL.h>
+#include "../libs/gif/gifload.h"
 
 int main(int argc, char *argv[]) {
 	return EXIT_SUCCESS;
